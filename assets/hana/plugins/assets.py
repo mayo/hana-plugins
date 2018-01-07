11fccb73af8e8a2ab6b15b3deba89730c6927b06
Mayo Jordanov <mayo@oyam.ca>
2018-01-07 11:40:05 -0800

    Add option to load files into Hana or sideloading
    Either load files into Hana file set (new addition) or sideloading as the plugin worked previously. This is useful for deployments without writing to disk or further file processing.

--- assets/hana/plugins/assets.py
+++ assets/hana/plugins/assets.py
@@ -1,17 +1,43 @@
-from hana.errors import HanaPluginError
 import os
+import logging
 import shutil
+
+from hana.core import FSFile
+from hana.errors import HanaPluginError
 
 # Add static assets to hana files
 
-def assets(assets, base_dir, create_dirs=True):
+def assets(assets, base_dir='', create_dirs=True, sideload=False):
+    logger = logging.getLogger(__name__)
 
-    def asset_plugin(files, hana):
+    def asset_load(files, hana):
         for src, dst in assets.items():
             if not dst:
                 raise InvalidAssetDefinition(src)
 
-            src = os.path.join(src)
+            dst = os.path.join(base_dir, dst)
+
+            if dst in files:
+                raise FileExistsError("File {} already exists".format(dst))
+
+            if os.path.isdir(src):
+                for path, _, sfiles in os.walk(src):
+                    sub_path = path[len(src):]
+
+                    for sfile in sfiles:
+                        src_path = os.path.join(path, sfile)
+                        dst_path = os.path.join(dst, sub_path, sfile)
+
+                        files.add(dst_path, FSFile(src_path))
+
+            else:
+                files.add(dst, FSFile(src))
+
+    def asset_sideload(files, hana):
+        for src, dst in assets.items():
+            if not dst:
+                raise InvalidAssetDefinition(src)
+
             dst = os.path.join(base_dir, dst)
 
             if create_dirs:
@@ -36,8 +62,17 @@ def assets(assets, base_dir, create_dirs=True):
             else:
                 shutil.copy(src, dst)
 
-    return asset_plugin
+    if sideload:
+        return asset_sideload
 
-class InvalidAssetDefinitionError(HanaPluginError): pass
+    return asset_load
 
+class AssetPluginError(HanaPluginError):
+    pass
+
+class InvalidAssetDefinitionError(AssetPluginError):
+    pass
+
+class FileExistsError(AssetPluginError):
+    pass
 
