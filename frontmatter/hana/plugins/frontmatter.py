1f8a09c6da5e9aa6f0ca2a122c006a96772cc555
Mayo Jordanov <mayo@oyam.ca>
2018-01-07 11:40:26 -0800

    Add debugging options

--- frontmatter/hana/plugins/frontmatter.py
+++ frontmatter/hana/plugins/frontmatter.py
@@ -1,14 +1,19 @@
 from __future__ import absolute_import
+import logging
+
 import frontmatter as fm
 
 def frontmatter(files, hana):
+    logger = logging.getLogger(__name__)
 
-    for _, f in files:
+    for filename, f in files:
         if f.is_binary:
             continue
 
         # This will strip empty front matter statement
         metadata, f['contents'] = fm.parse(f['contents'])
+
+        logger.debug('frontmatter %s', filename)
 
         if metadata:
             f.update(metadata)
