992399f784f59bc64517f24434f5fe6829563d80
Mayo Jordanov <mayo@oyam.ca>
2018-12-07 21:25:46 -0800

    Sass plugin

---
+++ sass/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from codecs import open
+from os import path
+
+basedir = path.abspath(path.dirname(__file__))
+
+setup(name='hana-plugin-sass',
+    version='0.0.1',
+
+    description='SASS plugin for Hana static site generator',
+
+    install_requires=[
+        'sass>=2.3.0',
+    ],
+
+    author='Mayo Jordanov',
+    author_email='mayo@oyam.ca',
+
+    url='https://github.com/mayo/hana-plugins',
+
+    license='MIT',
+
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+
+    keywords='hana sass metadata',
+
+    packages=['hana.plugins'],
+
+    entry_points={
+        'hana.plugins': [
+            'sass = hana.plugins.sass.Sass',
+        ]
+    }
+)
+
